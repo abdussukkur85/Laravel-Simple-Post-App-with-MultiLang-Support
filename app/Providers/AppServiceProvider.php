880d6eee133809c0713a7e5ec7e63c1b207ee369
abdussukkur85 <abdussukkur73@gmail.com>
2022-08-08 17:36:46 +0600

    remove laravel debugbar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,7 @@ class AppServiceProvider extends ServiceProvider {
      */
     public function register() {
         //
+
     }
 
     /**
@@ -23,6 +24,5 @@ class AppServiceProvider extends ServiceProvider {
      */
     public function boot() {
         Paginator::useBootstrap();
-        Debugbar::disable();
     }
 }
